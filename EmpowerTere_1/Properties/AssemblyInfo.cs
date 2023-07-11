b42efb1a861993a5b6b62ea0e9c1f4c08ef92665
Maria Correa <maria.correa@skyline.be>
2023-07-11 09:41:43 -0400

    Add project files.

---
+++ EmpowerTere_1/Properties/AssemblyInfo.cs
@@ -0,0 +1,16 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("EmpowerTere_1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Skyline Communications")]
+[assembly: AssemblyProduct("EmpowerTere_1")]
+[assembly: AssemblyCopyright("Copyright © Skyline Communications")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+[assembly: Guid("95197181-EC45-4572-8FFE-6FFF9AD5F623")]
+[assembly: AssemblyVersion("1.0.0.0")]
